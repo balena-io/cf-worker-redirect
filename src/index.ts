721ec03c9db0300579958a8dd2e8a977c97dc96c
Edwin Joassart <edwin@blck.coffee>
2025-01-24 22:10:26 +0100

    patch: switch to balena-lint

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import { handleRequest } from './handler'
+import { handleRequest } from './handler';
 
 addEventListener('fetch', (event) => {
-	const request = event.request
+	const request = event.request;
 
 	if (request.method === 'GET') {
-		event.respondWith(handleRequest(request)) // Handle target request
+		event.respondWith(handleRequest(request)); // Handle target request
 	} else {
 		event.respondWith(
 			new Response(null, {
 				status: 405,
-				statusText: 'Method Not Allowed'
-			})
-		)
+				statusText: 'Method Not Allowed',
+			}),
+		);
 	}
-})
+});
