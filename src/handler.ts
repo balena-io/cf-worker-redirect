9f35958b354182eede7f49aaa04a715c55c87d4f
Edwin Joassart <edwin@blck.coffee>
2025-01-25 09:08:04 +0100

    minor: handle variables in target path

--- src/handler.ts
+++ src/handler.ts
@@ -2,14 +2,9 @@ declare const REDIRECT_KV: KVNamespace; // estlint-disable-line no-undef
 
 export async function handleRequest(request: Request): Promise<Response> {
 	const requestUrl = new URL(request.url);
-	const requestPath = requestUrl.pathname;
-	const requestRootPath = `${requestUrl.host}/`;
 
 	// Get the matching target URL by first checking against the full url then host and then the path
-	const target =
-		(await REDIRECT_KV.get(`${requestUrl.host}${requestUrl.pathname}`)) ??
-		(await REDIRECT_KV.get(requestRootPath)) ??
-		(await REDIRECT_KV.get(requestPath));
+	const target = await getTargetfromKV(requestUrl);
 
 	if (!target || target === '404') {
 		// Optionally redirect to custom page on 404
@@ -44,12 +39,27 @@ export async function handleRequest(request: Request): Promise<Response> {
 
 	// Construct URL class from target (value from KV) so we can easily manipulate it
 	const targetUrl = new URL(target);
+
 	// Default to using targetUrl path
 	let redirectPath = targetUrl.pathname;
+
 	// Check if the request has a path though
-	if (!emptyPath(requestPath) && emptyPath(targetUrl.pathname)) {
+	if (!emptyPath(requestUrl.pathname) && emptyPath(targetUrl.pathname)) {
 		// Since request has a path but target does not, use requestPath
-		redirectPath = requestPath;
+		redirectPath = requestUrl.pathname;
+	}
+
+	// Check if the target has variables to be swapped out
+	if (redirectPath.includes('$')) {
+		try {
+			redirectPath = replaceVariables(redirectPath, requestUrl.pathname);
+		} catch (error: any) {
+			console.error('error', error.message);
+			return new Response('Sorry, page not found.', {
+				status: 404,
+				statusText: 'page not found',
+			});
+		}
 	}
 
 	// Construct a redirect url using target host and redirect path
@@ -68,4 +78,58 @@ export async function handleRequest(request: Request): Promise<Response> {
 
 function emptyPath(path: string): boolean {
 	return path === '/';
+}
+
+function replaceVariables(targetPath: string, requestPath: string): string {
+	const requestPathParts = requestPath.split('/');
+	const targetPathParts = targetPath.split('/');
+
+	// Loop through the parts to replace variables
+	for (let i = 0; i < targetPathParts.length; i++) {
+		if (targetPathParts[i].includes('$')) {
+			// Replace the variable with the actual value
+			const index = parseInt(targetPathParts[i].replace('$', ''), 10);
+			if (requestPathParts[index] !== undefined) {
+				targetPathParts[i] = requestPathParts[index];
+			} else {
+				throw new Error(
+					`Variable ${targetPathParts[i]} not found in request path`,
+				);
+			}
+		}
+	}
+
+	// Join the parts back together
+	return targetPathParts.join('/');
+}
+
+async function getTargetfromKV(requestUrl: URL): Promise<string> {
+	const requestPath = requestUrl.pathname;
+	const requestRootPath = `${requestUrl.host}/`;
+
+	const splittedPath = requestPath.split('/');
+
+	// construct an array of path
+	const requestPaths = [`${requestUrl.host}${splittedPath[0]}`];
+	for (let index = 1; index < splittedPath.length; index++) {
+		requestPaths.push(`${requestPaths[index - 1]}/${splittedPath[index]}`);
+	}
+
+	// reverse the array so we can check from the most specific path
+	requestPaths.reverse();
+
+	// add the root paths to the array
+	requestPaths.push(requestRootPath);
+	requestPaths.push(requestPath);
+
+	// test the route from the most specific path to the root path
+	for (const path of requestPaths) {
+		const target = await REDIRECT_KV.get(path);
+		if (target) {
+			// return then first target found (most specific)
+			return target;
+		}
+	}
+
+	return '404';
 }
